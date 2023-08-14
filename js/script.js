cd4ab41dbb805fbd00d39d704ee3d2d1a9d9b4ea
VISHAL KUMAR SINHA <vishalsinha211299@gmail.com>
2023-08-14 14:42:56 +0530

    commit
    ok

--- js/script.js
+++ js/script.js
@@ -1,148 +1,148 @@
 /*typing animation*/
-var typed = new Typed(".typing-1",{
-    strings:["", "Web Developer", "Backend Developer", "App Developer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
-var typed = new Typed(".typing-2",{
-    strings:["", "Web Developer", "Backend Developer", "App Developer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
+var typed = new Typed(".typing-1", {
+  strings: ["", "Web Developer", "Backend Developer", "App Developer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
+var typed = new Typed(".typing-2", {
+  strings: ["", "Web Developer", "Backend Developer", "App Developer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
 /*event*/
 var tablinks = document.getElementsByClassName("tab-item");
 var tabcontents = document.getElementsByClassName("tab-content");
 function opentab(tabname) {
-    for(tablink of tablinks) {
-        tablink.classList.remove("active");
-    }
-    for (tabcontent of tabcontents) {
-        tabcontent.classList.remove("active");
-    }
-    event.currentTarget.classList.add("active");
-    document.getElementById(tabname).classList.add("active"); 
+  for (tablink of tablinks) {
+    tablink.classList.remove("active");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active");
+  }
+  event.currentTarget.classList.add("active");
+  document.getElementById(tabname).classList.add("active");
 }
 /*aside animation*/
 const nav = document.querySelector(".nav"),
-navList =nav.querySelectorAll("li"),
-totalNavList = navList.length,
-allSection = document.querySelectorAll(".section"),
-totalSection = allSection.length;
-for(let i=0;i<totalNavList;i++)
-{
-    const a=navList[i].querySelector("a");
-    a.addEventListener("click", function()
-    {
-        removeBackSection();
-        for(let j=0;j<totalNavList;j++)
-        {
-            if(navList[j].querySelector("a").classList.contains("active"))
-            {
-                addBackSection(j);
-            }
-            navList[j].querySelector("a").classList.remove("active");
-        }
-        this.classList.add("active")
-        showSection(this);
-        if(window.innerWidth <1200)
-        {
-            asideSectionTogglerBtn();
-        }
-    })
-}
-function removeBackSection()
-{
-    for(let i=0;i<totalSection;i++)
-    {
-        allSection[i].classList.remove("back-section");
+  navList = nav.querySelectorAll("li"),
+  totalNavList = navList.length,
+  allSection = document.querySelectorAll(".section"),
+  totalSection = allSection.length;
+for (let i = 0; i < totalNavList; i++) {
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    removeBackSection();
+    for (let j = 0; j < totalNavList; j++) {
+      if (navList[j].querySelector("a").classList.contains("active")) {
+        addBackSection(j);
+      }
+      navList[j].querySelector("a").classList.remove("active");
     }
-}
-function addBackSection(num)
-{
-    allSection[num].classList.add("back-section");
-}
-function showSection(element)
-{
-    for(let i=0;i<totalSection;i++)
-    {
-        allSection[i].classList.remove("active");
+    this.classList.add("active");
+    showSection(this);
+    if (window.innerWidth < 1200) {
+      asideSectionTogglerBtn();
     }
+  });
+}
+function removeBackSection() {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("back-section");
+  }
+}
+function addBackSection(num) {
+  allSection[num].classList.add("back-section");
+}
+function showSection(element) {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("active");
+  }
+  const target = element.getAttribute("href").split("#")[1];
+  document.querySelector("#" + target).classList.add("active");
+}
+function updateNav(element) {
+  for (let i = 0; i < totalNavList; i++) {
+    navList[i].querySelector("a").classList.remove("active");
     const target = element.getAttribute("href").split("#")[1];
-    document.querySelector("#" + target).classList.add("active")
-}
-function updateNav(element)
-{
-    for(let i=0;i<totalNavList;i++)
-    {
-        navList[i].querySelector("a").classList.remove("active");
-        const target = element.getAttribute("href").split("#")[1];
-        if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
-        {
-            navList[i].querySelector("a").classList.add("active");
-        }
+    if (
+      target ===
+      navList[i].querySelector("a").getAttribute("href").split("#")[1]
+    ) {
+      navList[i].querySelector("a").classList.add("active");
     }
+  }
 }
 /*buttons*/
-document.querySelector(".logo-main").addEventListener("click", function()
-{
-    const sectionIndex = this.getAttribute("data-section-index");
-    showSection(this);
-    updateNav(this);
-    removeBackSection();
-    addBackSection(sectionIndex);
-})
-document.querySelector(".hire-me").addEventListener("click", function()
-{
-    const sectionIndex = this.getAttribute("data-section-index");
-    showSection(this);
-    updateNav(this);
-    removeBackSection();
-    addBackSection(sectionIndex);
-})
-document.querySelector(".about-me").addEventListener("click", function()
-{
-    const sectionIndex = this.getAttribute("data-section-index");
-    showSection(this);
-    updateNav(this);
-    removeBackSection();
-    addBackSection(sectionIndex);
-})
+document.querySelector(".logo-main").addEventListener("click", function () {
+  const sectionIndex = this.getAttribute("data-section-index");
+  showSection(this);
+  updateNav(this);
+  removeBackSection();
+  addBackSection(sectionIndex);
+});
+document.querySelector(".hire-me").addEventListener("click", function () {
+  const sectionIndex = this.getAttribute("data-section-index");
+  showSection(this);
+  updateNav(this);
+  removeBackSection();
+  addBackSection(sectionIndex);
+});
+document.querySelector(".about-me").addEventListener("click", function () {
+  const sectionIndex = this.getAttribute("data-section-index");
+  showSection(this);
+  updateNav(this);
+  removeBackSection();
+  addBackSection(sectionIndex);
+});
 const navTogglerBtn = document.querySelector(".nav-toggler"),
-aside = document.querySelector(".aside");
-navTogglerBtn.addEventListener("click", () =>
-{
-    asideSectionTogglerBtn();
-})
-function asideSectionTogglerBtn()
-{
-    aside.classList.toggle("open");
-    navTogglerBtn.classList.toggle("open");
-    for(let i=0;i<totalSection;i++)
-    {
-        allSection[i].classList.toggle("open");
-    }
+  aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => {
+  asideSectionTogglerBtn();
+});
+function asideSectionTogglerBtn() {
+  aside.classList.toggle("open");
+  navTogglerBtn.classList.toggle("open");
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.toggle("open");
+  }
 }
 
 /*Swiper*/
-const swiper = new Swiper('.swiper', {
-    autoHeight: true,
-    loop: true,
+const swiper = new Swiper(".swiper", {
+  autoHeight: true,
+  loop: true,
 
-    // If we need pagination
-    pagination: {
-    el: '.swiper-pagination',
-    },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
 
-    // Navigation arrows
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-    // And if we need scrollbar
-    scrollbar: {
-    el: '.swiper-scrollbar',
-    },
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+//form
+const scriptURL = "https://script.google.com/macros/s/AKfycbzILCWuXnvi-VFKg6DwL3IrpQSZp7aNbkha4IoPN5Zu_7zE5KO3xIGQVMjfe6dsah4A/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully!";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
 });
